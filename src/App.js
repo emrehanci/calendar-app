050970565eaf5160474073aa8f9809210aaafa4a
Emre Hanci <emre.hanci@icloud.com>
2025-08-07 18:32:15 +0300

    Json Server

--- src/App.js
+++ src/App.js
@@ -1,53 +1,114 @@
 import React, { useEffect, useState } from 'react';
-import { Calendar, Modal, Form, Select, DatePicker, Button, Spin } from 'antd';
+import { Calendar, Modal, Form, Select, DatePicker, Button, Spin, message, Popconfirm } from 'antd';
 import dayjs from 'dayjs';
-import axios from 'axios';
 import isBetween from 'dayjs/plugin/isBetween';
-
-dayjs.extend(isBetween);
+import axios from 'axios';
+import { v4 as uuidv4 } from 'uuid';
+const { confirm } = Modal;
 const { Option } = Select;
+dayjs.extend(isBetween);
 
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [editMode, setEditMode] = useState(false);
   const [form] = Form.useForm();
   const [events, setEvents] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState([]);
-  const [filters, setFilters] = useState({});
   const [dropdownData, setDropdownData] = useState(null);
+  const [selectedEventId, setSelectedEventId] = useState(null);
 
   useEffect(() => {
-    axios.get('/events.json').then(res => {
-      setEvents(res.data);
-      setFilteredEvents(res.data);
-    });
-
+    fetchEvents();
     axios.get('/dropdowns.json').then(res => setDropdownData(res.data));
   }, []);
 
-  const handleAdd = values => {
+  const fetchEvents = async () => {
+    const res = await axios.get('http://localhost:3001/events');
+    setEvents(res.data);
+    setFilteredEvents(res.data);
+  };
+
+  const createEvent = async (event) => {
+    await axios.post('http://localhost:3001/events', event);
+  };
+  
+  const updateEvent = async (id, event) => {
+    await axios.put(`http://localhost:3001/events/${id}`, event);
+  };
+  
+  const deleteEvent = async (id) => {
+    await axios.delete(`http://localhost:3001/events/${id}`);
+  };
+
+  const handleAddOrUpdate = async values => {
     const newEvent = {
+      id: editMode && selectedEventId ? selectedEventId : uuidv4(),
       ...values,
       start: values.start.format('YYYY-MM-DD'),
       end: values.end.format('YYYY-MM-DD'),
     };
-    const updatedEvents = [...events, newEvent];
+
+    let updatedEvents = [];
+
+    if (editMode && selectedEventId) {
+      updatedEvents = events.map(e => (e.id === selectedEventId ? newEvent : e));
+      message.success('Event updated');
+    } else {
+      updatedEvents = [...events, newEvent];
+      message.success('Event added');
+    }
+
     setEvents(updatedEvents);
     setFilteredEvents(updatedEvents);
+    if (editMode && selectedEventId) {
+      await updateEvent(selectedEventId, newEvent);
+    } else {
+      await createEvent(newEvent);
+    }
     setModalVisible(false);
     form.resetFields();
+    setEditMode(false);
+    setSelectedEventId(null);
   };
 
   const handleFilter = person => {
     setFilteredEvents(events.filter(e => e.name === person));
   };
 
+  const onUpdate = (event) => {
+    form.setFieldsValue({
+      ...event,
+      start: dayjs(event.start),
+      end: dayjs(event.end)
+    });
+    setModalVisible(true);
+    setEditMode(true);
+    setSelectedEventId(event.id);
+  }
+
+  const onDelete = async (event) => {
+    const updated = events.filter(e => e.id !== event.id);
+    setEvents(updated);
+    setFilteredEvents(updated);
+    await deleteEvent(event.id);
+    message.success('Event deleted');
+  }
+
   const dateCellRender = value => {
     const currentDate = value.format('YYYY-MM-DD');
     const dayEvents = filteredEvents.filter(e => dayjs(currentDate).isBetween(e.start, e.end, null, '[]'));
     return (
       <ul style={{ listStyle: 'none', padding: 0 }}>
-        {dayEvents.map((item, idx) => (
-          <li key={idx}><strong>{item.name}</strong> - {item.type}</li>
+        {dayEvents.map((item) => (
+          <Popconfirm
+          title="Do you want to update or delete this event?"
+          onConfirm={() => onDelete(item)}
+          onCancel={() => onUpdate(item)}
+          okText="Delete"
+          cancelText="Update"
+        >
+          <strong>{item.name}</strong> - {item.type}
+        </Popconfirm>
         ))}
       </ul>
     );
@@ -69,16 +130,20 @@ const App = () => {
         </Select>
         <Button type="primary" style={{ marginLeft: 16 }} onClick={() => setModalVisible(true)}>Add New Entry</Button>
       </div>
-      <Calendar dateCellRender={dateCellRender} />
+      <Calendar cellRender={dateCellRender} />
 
       <Modal
-        title="Add New Holiday"
+        title={editMode ? "Update Event" : "Add New Holiday"}
         open={modalVisible}
-        onCancel={() => setModalVisible(false)}
+        onCancel={() => {
+          setModalVisible(false);
+          form.resetFields();
+          setEditMode(false);
+        }}
         onOk={() => form.submit()}
         okText="Submit"
       >
-        <Form form={form} layout="vertical" onFinish={handleAdd}>
+        <Form form={form} layout="vertical" onFinish={handleAddOrUpdate}>
           <Form.Item name="name" label="Name" rules={[{ required: true }]}> 
             <Select>
               {dropdownData.names.map(name => <Option key={name} value={name}>{name}</Option>)}
